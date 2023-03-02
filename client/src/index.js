0361dbc5a0535680e2a0fa7c6992fc713257e76e
Aleksander <asava@sibmail.com>
2023-03-02 11:58:57 +0700

    нет билда

---
+++ client/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './app/App'
+import { Router } from 'react-router-dom'
+import { createStore } from './app/store/createStore'
+import { Provider } from 'react-redux'
+import history from './app/utils/history'
+
+const store = createStore()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+    <Provider store={store}>
+        <Router history={history}>
+            <App />
+        </Router>
+    </Provider>
+</React.StrictMode>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
+
+// warning: ReactDOM.render is no longer supported in React 18.
+// Use createRoot instead. Until you switch to the new API,
+// your app will behave as if it's running React 17. Learn more
+// =================================================
+// ---
+// import ReactDOM from 'react-dom'
+// ---
+// ----
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Provider store={store}>
+//             <Router history={history}>
+//                 <App />
+//             </Router>
+//         </Provider>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// )
+// =============================================
